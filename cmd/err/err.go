08179e6c957ec1842f8191e9a3745ae9d22fc6ff
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-03-24 13:59:51 -0300

    expand on errors

--- cmd/err/err.go
+++ cmd/err/err.go
@@ -4,20 +4,30 @@ import (
 	"fmt"
 )
 
-type Temporary struct {
-	Temporary bool
-	message   string
+type Error struct {
+	Temporary  bool
+	BadRequest bool
+	message    string
 }
 
-func (e *Temporary) Error() string {
+func (e *Error) Error() string {
 	return fmt.Sprintf("%s", e.message)
 }
 
-func NewTemporaryError(message string) *Temporary {
-	return &Temporary{Temporary: true, message: message}
+func NewTemporaryError(message string) *Error {
+	return &Error{Temporary: true, message: message}
+}
+
+func NewBadRequestError(message string) *Error {
+	return &Error{BadRequest: true, message: message}
 }
 
 func IsTemporary(err error) bool {
-	te, ok := err.(*Temporary)
+	te, ok := err.(*Error)
 	return ok && te.Temporary
+}
+
+func IsBadRequest(err error) bool {
+	te, ok := err.(*Error)
+	return ok && te.BadRequest
 }
