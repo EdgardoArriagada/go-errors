3330dee9c51dbc4c9348ffeca79b6fccf2f11dc0
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-03-24 15:47:35 -0300

    marry with 2d approach

--- cmd/main.go
+++ cmd/main.go
@@ -7,12 +7,15 @@ import (
 )
 
 func function2() error {
-	return nerr.NewBadRequestError("bad request error")
+	// some external error
+	return errors.New("some external error")
 }
 
 func function1() error {
+	// wrap it making it temporary
 	if err := function2(); err != nil {
-		return errors.Join(err, errors.New("function2 failed"))
+		error := errors.New("at function 1")
+		return errors.Join(err, error, nerr.TemporaryError)
 	}
 
 	return nil
@@ -20,30 +23,7 @@ func function1() error {
 
 func function0() error {
 	if err := function1(); err != nil {
-		return errors.Join(err, errors.New("function1 failed"))
-	}
-
-	return nil
-}
-
-func function22() error {
-	// some external error
-	return errors.New("some external error")
-}
-
-func function11() error {
-	// wrap it making it temporary
-	if err := function22(); err != nil {
-		error := errors.New("at function 11")
-		return errors.Join(err, error, nerr.TemporaryError)
-	}
-
-	return nil
-}
-
-func function00() error {
-	if err := function11(); err != nil {
-		return errors.Join(err, errors.New("at function00"))
+		return errors.Join(err, errors.New("at function 0"))
 	}
 
 	return nil
@@ -51,22 +31,19 @@ func function00() error {
 
 func main() {
 	normalError := errors.New("normal error")
-	temporaryError := nerr.NewTemporaryError("some temporary error")
+	temporalError := function0()
 
-	fmt.Println("with normal error", nerr.IsTemporary(normalError))
-	fmt.Println("with temporary error", nerr.IsTemporary(temporaryError))
+	fmt.Println("le approach2Error", temporalError)
 
-	// printing messages
-	fmt.Println("with normal error:", normalError)
-	fmt.Println("with temporary error:", temporaryError)
+	if errors.Is(temporalError, nerr.TemporaryError) {
+		fmt.Println("'temporalError' is temporary error!")
+	} else {
+		fmt.Println("'temporalError' is NOT temporary error.")
+	}
 
-	badRequestError := function0()
-	fmt.Println("with bad request error:", badRequestError)
-
-	approach2Error := function00()
-	fmt.Println("le approach2Error", approach2Error)
-
-	if errors.Is(approach2Error, nerr.TemporaryError) {
-		fmt.Println("is temporary error!")
+	if errors.Is(normalError, nerr.TemporaryError) {
+		fmt.Println("'normalError' is temporary error!")
+	} else {
+		fmt.Println("'normalError' is NOT temporary error.")
 	}
 }
--- cmd/nerr/nerr.go
+++ cmd/nerr/nerr.go
@@ -1,41 +1,5 @@
 package nerr
 
-import (
-	"fmt"
-)
-
-/// Approach 1
-
-type Error struct {
-	Temporary  bool
-	BadRequest bool
-	message    string
-}
-
-func (e *Error) Error() string {
-	return fmt.Sprintf("%s", e.message)
-}
-
-func NewTemporaryError(message string) *Error {
-	return &Error{Temporary: true, message: message}
-}
-
-func NewBadRequestError(message string) *Error {
-	return &Error{BadRequest: true, message: message}
-}
-
-func IsTemporary(err error) bool {
-	te, ok := err.(*Error)
-	return ok && te.Temporary
-}
-
-func IsBadRequest(err error) bool {
-	te, ok := err.(*Error)
-	return ok && te.BadRequest
-}
-
-/// Approach 2
-
 type temporaryError struct {
 	message string
 }
